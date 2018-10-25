e4b9a000d8804fada1482384e76a6c678d0f721b
Nicholas Vigil <42006369+vigilne@users.noreply.github.com>
2018-10-25 18:09:49 -0500

    fortigate firewall config backup script

---
+++ fortinet/fortigate_backup.py
@@ -0,0 +1,44 @@
+from netmiko import ConnectHandler
+from netmiko.ssh_exception import NetMikoTimeoutException
+from paramiko.ssh_exception import SSHException
+from netmiko.ssh_exception import AuthenticationException
+import time
+
+with open('fwip_list.txt') as f:
+    device_list = f.read().splitlines()
+
+for devices in device_list:
+
+    ip_address = devices
+    firewall = {
+        'device_type': 'fortinet',
+        'ip': ip_address,
+        'username': 'fitroot',
+        'password': 'Thr3@dNeed!3',
+    }
+    
+    timestr = time.strftime("%Y%m%d") 
+
+    try:
+        net_connect = ConnectHandler(**firewall)
+    except (AuthenticationException):
+        continue
+    except (NetMikoTimeoutException):
+        continue
+    except (EOFError):
+        continue
+    except (SSHException):
+        continue
+    except Exception as unknown_error:
+        continue
+
+    output = net_connect.send_command("show full-configuration", delay_factor=2)
+
+    backup_path = '/usr/backups/fortinet/%s' % (firewall['ip'])
+
+    filename = backup_path + str("_backupfile_"+ timestr)
+    f = open(filename, 'w+')
+    f.write(output)
+    f.close()
+
+    net_connect.disconnect()
